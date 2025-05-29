601865d5e4e92a27d8b3c11874e6156d2a69bca6
Pathstar <114277040+Pathstar@users.noreply.github.com>
2025-05-29 17:27:15 +0800

    Delete super directory

--- super/super4.py
+++
@@ -1,8 +0,0 @@
-class A:
-    def __init__(self):
-        print("A的初始化方法被调用")
-class B(A):
-    def __init__(self):
-        super().__init__()  # 显式调用父类的__init__
-        print("B的初始化方法被调用")
-b = B()  # 输出: A的初始化方法被调用 → B的初始化方法被调用
